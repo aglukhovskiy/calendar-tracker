5fc0fc62078188d7c2ad045cb71839ae6228e8a8
aglukhovskiy <152854712+aglukhovskiy@users.noreply.github.com>
2025-06-08 10:59:37 +0000

    Deploying to gh-pages from @ aglukhovskiy/calendar-tracker@81628161bf0a7f55e77d12aa4e5f3b3b7231458f 🚀

--- main.bundle.js
+++ main.bundle.js
@@ -8281,8 +8281,8 @@ const createClient = (supabaseUrl, supabaseKey, options) => {
 ;// ./config.js
 // Получите эти значения из настроек вашего проекта в Supabase
 // Project Settings -> API -> Project URL и anon/public key
-const SUPABASE_URL = undefined;
-const SUPABASE_ANON_KEY = undefined;
+const SUPABASE_URL = "https://mdzamxwkylxcqxyshxfi.supabase.co" || 0;
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kemFteHdreWx4Y3F4eXNoeGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5ODU2MDksImV4cCI6MjA2NDU2MTYwOX0.9rNKeuKixlvOTigZKPLurGozMhzJ9X_u7ujJXvSaz7A" || 0;
 
 // Имена таблиц в базе данных
 const TABLES = {
