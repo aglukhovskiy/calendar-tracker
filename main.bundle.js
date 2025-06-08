5fff4ec05eabe5f42b6104d83e49b972a7a3b2e2
aglukhovskiy <152854712+aglukhovskiy@users.noreply.github.com>
2025-06-08 20:12:12 +0000

    Deploying to gh-pages from @ aglukhovskiy/calendar-tracker@2cce243c63d09165525d4158199f65aa8b7a8c13 🚀

--- main.bundle.js
+++ main.bundle.js
@@ -10385,6 +10385,13 @@ function closeEventModal() {
     toggleCompletionBtn.onclick = null;
   }
 }
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : r & 0x3 | 0x8;
+    return v.toString(16);
+  });
+}
 async function saveEvent(eventData) {
   try {
     // Преобразуем время в нужный формат
@@ -10407,6 +10414,7 @@ async function saveEvent(eventData) {
       await db.updateCalendarEvent(eventData.id, eventDataToSave);
     } else {
       // Создание нового события
+      eventDataToSave.id = generateUUID();
       await db.createCalendarEvent(eventDataToSave);
     }
 
