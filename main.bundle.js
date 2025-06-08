1f23b553a1e83a226dddca7520ad8c206cbc728f
aglukhovskiy <152854712+aglukhovskiy@users.noreply.github.com>
2025-06-08 11:56:35 +0000

    Deploying to gh-pages from @ aglukhovskiy/calendar-tracker@f563a716032fd018107a1aa72a317d95630ef3b2 🚀

--- main.bundle.js
+++ main.bundle.js
@@ -9136,38 +9136,6 @@ function updateTotalCaloriesDisplay() {
   const evening = parseInt(dom_elements_elements.caloriesEveningInput.value) || 0;
   dom_elements_elements.totalCaloriesValueSpan.textContent = (morning + afternoon + evening).toString();
 }
-
-// Слушатели событий
-// [caloriesMorningInput, caloriesAfternoonInput, caloriesEveningInput].forEach(input => {
-//     if (input) input.addEventListener('input', updateTotalCaloriesDisplay);
-// });
-
-// if (saveDayDetailsBtn) {
-//     saveDayDetailsBtn.addEventListener('click', async () => {
-//         const dateStr = dayDetailModalDateDisplay.textContent;
-//         if (!dateStr) {
-//             alert("Ошибка: не удалось определить дату для сохранения.");
-//             return;
-//         }
-
-//         const detailsPayload = {
-//             calories: {
-//                 morning: parseInt(caloriesMorningInput.value, 10) || 0,
-//                 afternoon: parseInt(caloriesAfternoonInput.value, 10) || 0,
-//                 evening: parseInt(caloriesEveningInput.value, 10) || 0,
-//             },
-//             comment: commentInput.value.trim()
-//         };
-
-//         await saveDayDetails(dateStr, detailsPayload);
-//         closeDayDetailModal();
-//     });
-// }
-
-// if (cancelDayDetailsBtn) {
-//     cancelDayDetailsBtn.addEventListener('click', closeDayDetailModal);
-// }
-
 async function loadDayDetails() {
   const result = await storage_storage.get(ALL_DAY_DETAILS_KEY);
   allDayDetailsData = result[ALL_DAY_DETAILS_KEY] || {};
